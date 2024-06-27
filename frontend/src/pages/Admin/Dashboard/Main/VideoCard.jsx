965082da8a5d41758afea82a452aef004c5c2225
NaveenReddy0427 <naveendeva909@gmail.com>
2024-06-27 19:56:26 +0530

    created movie web application

---
+++ frontend/src/pages/Admin/Dashboard/Main/VideoCard.jsx
@@ -0,0 +1,34 @@
+
+
+import PropTypes from 'prop-types';
+
+const VideoCard = ({ image, title, date, comments }) => {
+  return (
+    <>
+      <div className="flex items-center w-[90%] mt-5">
+        <div>
+          <img src={image} alt="Card Image" className="h-[3rem]" />
+        </div>
+
+        <div className="ml-4">
+          <h2 className="text-lg text-white">{title}</h2>
+          <p className="text-gray-500 mb-3">{date}</p>
+        </div>
+
+        <div className="flex-grow mb-5 flex justify-end items-center">
+          <div className="text-white text-lg">{comments}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+VideoCard.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  date: PropTypes.string,
+  comments: PropTypes.number,
+};
+
+export default VideoCard;
+  
